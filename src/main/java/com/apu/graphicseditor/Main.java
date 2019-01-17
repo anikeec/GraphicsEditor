41ebcd53f6e9614bd04a9124f660e09f1fb561ef
apu <pasha_anik@ukr.net>
2019-01-17 15:53:32 +0200

    Refactoring

--- src/main/java/com/apu/graphicseditor/Main.java
+++ src/main/java/com/apu/graphicseditor/Main.java
@@ -16,7 +16,7 @@ import javafx.stage.Stage;
  *
  * @author apu
  */
-public class GraphicsEditor extends Application {
+public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
